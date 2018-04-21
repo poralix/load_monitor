881bddbdd02f4f49baae041cb029e78466fd96d0
root <root@good.relation.at>
2018-04-21 11:08:23 +0200

    Fixed an installation script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-/usr/local/directadmin/plugins/load_monitor/scripts/install_db.sh
+
+chown -R diradmin:diradmin /usr/local/directadmin/plugins/load_monitor/;
+/usr/local/directadmin/plugins/load_monitor/scripts/install_db.sh;
 echo 'Read the Help section inside the Load Monitor plugin for further instructions or nothing will happen :)';
 exit 0;
