6fa271938ffb1a7b2f5fff86e3951bdfbb7fa446
root <root@good.relation.at>
2018-04-21 11:16:47 +0200

    Crontab task is now added at install time

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,5 +2,6 @@
 
 chown -R diradmin:diradmin /usr/local/directadmin/plugins/load_monitor/;
 /usr/local/directadmin/plugins/load_monitor/scripts/install_db.sh;
-echo 'Read the Help section inside the Load Monitor plugin for further instructions or nothing will happen :)';
+/usr/local/directadmin/plugins/load_monitor/scripts/install_cron.sh;
+#echo 'Read the Help section inside the Load Monitor plugin for further instructions or nothing will happen :)';
 exit 0;
