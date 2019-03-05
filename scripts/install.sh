3e4eb3aa666ff9e12848ab0f49520dd2771c6c6e
Alex <alexey@nsk21.ru>
2019-03-05 11:41:41 +0700

    Re-ordered of installation steps

--- scripts/install.sh
+++ scripts/install.sh
@@ -17,25 +17,28 @@
 PLUGIN_DIR="/usr/local/directadmin/plugins/load_monitor";
 PHP_INI="${PLUGIN_DIR}/php.ini";
 
-chmod 755 "${PLUGIN_DIR}";
+# permissions
 chown -R diradmin:diradmin "${PLUGIN_DIR}";
+chmod 755 "${PLUGIN_DIR}";
 chmod 755 ${PLUGIN_DIR}/admin/*.html;
 chmod 755 ${PLUGIN_DIR}/exec/*.sh;
-
-[ -f "${PLUGIN_DIR}/data/load_monitor.sqlite" ] || ${PLUGIN_DIR}/scripts/install_db.sh;
-${PLUGIN_DIR}/scripts/install_cron.sh;
-
-TZ=$(grep ^php_timezone= /usr/local/directadmin/custombuild/options.conf | cut -d= -f2);
-perl -pi -e "s#^date.timezone = .*#date.timezone = ${TZ}#" ${PHP_INI};
 
 # config
 CUSTOM_CONFIG="${PLUGIN_DIR}/exec/config.ini";
 DEFAULT_CONFIG="${PLUGIN_DIR}/exec/config.ini.default";
 [ -e "${CUSTOM_CONFIG}" ] || cp -p "${DEFAULT_CONFIG}" "${CUSTOM_CONFIG}";
-
 chown diradmin:diradmin "${CUSTOM_CONFIG}";
 chmod 644 "${CUSTOM_CONFIG}";
 
+# php.ini
+TZ=$(grep ^php_timezone= /usr/local/directadmin/custombuild/options.conf | cut -d= -f2);
+perl -pi -e "s#^date.timezone = .*#date.timezone = ${TZ}#" ${PHP_INI};
+
+# sqlite
+[ -f "${PLUGIN_DIR}/data/load_monitor.sqlite" ] || ${PLUGIN_DIR}/scripts/install_db.sh;
+${PLUGIN_DIR}/scripts/install_cron.sh;
+
+# wrapper
 GCC=gcc;
 if [ -e /usr/bin/gcc ]; then
     GCC=/usr/bin/gcc;
@@ -50,6 +53,7 @@ ${GCC} -std=gnu99 -B/usr/bin -o ${PLUGIN_DIR}/exec/${WRAPPER} ${PLUGIN_DIR}/exec
 chown root:diradmin ${PLUGIN_DIR}/exec/${WRAPPER};
 chmod 4550 ${PLUGIN_DIR}/exec/${WRAPPER};
 
+# plugin's configs
 cd "${PLUGIN_DIR}";
 perl -pi -e "s/^installed=.*/installed=yes/" plugin.conf;
 perl -pi -e "s/^active=.*/active=yes/" plugin.conf;
